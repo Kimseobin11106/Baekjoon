8990e8bc062206bd581bdbc39ab118205cfe840d
Kimseobin <128437643+Kimseobin11106@users.noreply.github.com>
2025-04-07 15:51:02 +0900

    Add files via upload

---
+++ Baekjoon1.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Baekjoon1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a,b;
+        
+        b = sc.nextInt();
+        a = sc.nextInt();
+
+        System.out.println(a+b);
+    }
+}
